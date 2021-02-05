124c0573b4bdd98116d54cc7cf7970c41163be59
luicahleo <luisrolandocahuanaleon@gmail.com>
2021-02-05 01:28:40 +0100

    105 maquetado perfiles.edit; completado metodo perfiles.update   pero falta el policy;
    subida de imagen y hechas las dos actualizaciones, uno en user y otro en perfils

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Perfil;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class PerfilController extends Controller
 {
@@ -27,7 +28,7 @@ class PerfilController extends Controller
      */
     public function edit(Perfil $perfil)
     {
-        //
+        return view('perfiles.edit', compact('perfil'));
     }
 
     /**
@@ -39,8 +40,50 @@ class PerfilController extends Controller
      */
     public function update(Request $request, Perfil $perfil)
     {
-        //
-    }
+        // Ejecutar el Policy
+        //$this->authorize('update', $perfil);
+
+        // Validar
+        $data = request()->validate([
+            'nombre' => 'required',
+            'url' => 'required',
+            'biografia' => 'required'
+        ]);
+
+        // Si el usuario sube una imagen
+        if( $request['imagen'] ) {
+            // obtener la ruta de la imagen
+            $ruta_imagen = $request['imagen']->store('upload-perfiles', 'public');
+
+            // Resize de la imagen
+            $img = Image::make( public_path("storage/{$ruta_imagen}"))->fit(600, 600 );
+            $img->save();
+
+            // Crear un arreglo de imagen
+            $array_imagen = ['imagen' => $ruta_imagen];
+        }
+
+        // Asignar nombre y URL
+        auth()->user()->url = $data['url'];
+        auth()->user()->name = $data['nombre'];
+        auth()->user()->save();
+
+        // Eliminar url y name de $data, para poder guardar datos en perfiles
+        // porque en perfiles no existe esos dos campos
+        unset($data['url']);
+        unset($data['nombre']);
+
+
+        // Guardar información
+        // Asignar Biografia e imagen
+        auth()->user()->perfil()->update( array_merge( //une dos array, en este caso, $data y $arra_imagen
+            $data,
+            $array_imagen ?? [] //si no existe array_imagen, entonces ponle un array vacio
+        ) );
+
+
+        // redireccionar
+        return redirect()->action('RecetaController@index');    }
 
     /**
      * Remove the specified resource from storage.
--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,8 @@ Route::put('/recetas/{receta}', 'RecetaController@update')->name('recetas.update
 Route::delete('/recetas/{receta}', 'RecetaController@destroy')->name('recetas.destroy');
 
 Route::get('/perfiles/{perfil}', 'PerfilController@show')->name('perfiles.show');
+Route::get('/perfiles/{perfil}/edit', 'PerfilController@edit')->name('perfiles.edit');
+Route::put('/perfiles/{perfil}', 'PerfilController@update')->name('perfiles.update');
 
 
 Auth::routes();
